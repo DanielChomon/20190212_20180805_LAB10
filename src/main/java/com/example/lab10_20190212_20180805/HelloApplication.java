e300d4c146f0e547ad7739aab068f904d572039b
Daniel Chomon <a20180805@pucp.edu.pe>
2022-06-27 19:41:29 -0500

    archivos creados 2

---
+++ src/main/java/com/example/lab10_20190212_20180805/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.lab10_20190212_20180805;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
